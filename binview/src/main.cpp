41171d72e4b840d0b85ab5022d4aa159042d2d51
nvxden <clever411@yandex.ru>
2021-06-21 17:58:25 +0300

    View v1.1, Add flag for binary output

--- binview/src/main.cpp
+++ binview/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 
 
@@ -8,11 +9,20 @@ using namespace std;
 
 
 // functions
-char val(int i)
+char hexval(int i)
 {
 	if(i < 10)
 		return '0' + i;
 	return 'a' + (i - 10);
+}
+
+string binval(uint8_t one)
+{
+	string res;
+	res.resize(8);
+	for (int i = 0; i < 8; ++i)
+		res[i] = one & (1 << (7-i)) ? '1' : '0';
+	return res.substr(0, 4) + "'" + res.substr(4);
 }
 
 
@@ -22,15 +32,28 @@ char val(int i)
 // main
 int main( int argc, char *argv[] )
 {
-	int c = 0;
-	int ch;
+	char mode = 'h';
 
-	while((ch = getchar()) != EOF)
+	for (int i = 1; i < argc; ++i)
 	{
-		printf("%c%c ", val(ch / 16), val(ch % 16));
+		if (!strcmp("-h", argv[i]) || !strcmp("--hex", argv[i]))
+			mode = 'h';
+		else if (!strcmp("-b", argv[i]) || !strcmp("--bin", argv[i]))
+			mode = 'b';
+	}
+
+	int c = 0;
+	uint8_t ch;
+	while(cin.read((char *)&ch, 1))
+	{
+		if (mode == 'h')
+			printf("%c%c ", hexval(ch / 16), hexval(ch % 16));
+		else
+			printf("%s ", binval(ch).c_str());
+
 		++c;
 		if(c % 4 == 0)
-			printf("\n");
+			putchar('\n');
 	}
 
 
